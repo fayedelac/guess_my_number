cf8d83eab19f3dc160ef732b201d07b181cafa3b
fayedelac <faye.delacretaz@epfl.ch>
2020-10-26 16:29:24 +0100

    Transformation du code avec l’ajout de la classe GuessMachine

--- guess_number.py
+++ guess_number.py
@@ -10,20 +10,52 @@ import random
 
 MIN = 0
 MAX = 1000
-number_to_guess = random.randint(MIN, MAX)
 
-print('Hey! Try to guess a number between %d and %d!' % (MIN, MAX))
 
-while True:
-    user_input = input('Your guess? ')
-    try:
-        user_attempt = int(user_input)
-        if user_attempt == number_to_guess:
-            print('Fantastic, you coud find the number I had in mind!')
-            break
-        elif user_attempt < number_to_guess:
-            print('too low')
+class GuessMachine():
+    '''
+    I have a number in mind,
+    you have to guess it!!
+    + self._number_to_guess is generated durinig creation of the object
+    + Use `guess(num)` method to make a guess
+    + I'll count the number ot attempt in self.number_of_attempt
+    '''
+
+    def __init__(self):
+
+        self._number_to_guess = random.randint(MIN, MAX)
+        self.number_of_attempt = 0
+
+    def guess(self, num):
+        '''
+        Attempt to find the right number
+        it returns 'too high', 'too low', 'found'
+        '''
+        self.number_of_attempt += 1
+        if num < self._number_to_guess:
+            return 'too low'
+        elif num > self._number_to_guess:
+            return 'too high'
         else:
-            print('too high')
-    except ValueError:
-        print('You joker ... that was not an integer!')
+            return 'found'
+
+
+if __name__ == '__main__':
+    guess_nachine = GuessMachine()
+    print('Hey! Try to guess a number between %d and %d!' % (MIN, MAX))
+
+    while True:
+        user_input = input('Your guess? ')
+        try:
+            user_attempt = int(user_input)
+            result = guess_nachine.guess(user_attempt)
+            if result == 'found':
+                print(
+                    'Fantastic, you coud find '
+                    'the number I had in mind '
+                    'in %d attempts!' % guess_nachine.number_of_attempt)
+                break
+            else:
+                print(result)
+        except ValueError:
+            print('You joker ... that was not an integer!')
